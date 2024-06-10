0f92b7770b4c1346acf449ca9a269b60457bbd4a
Fábio Pinto Figueiredo <pf95fabio@gmail.com>
2024-06-10 16:48:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+function resumo(){
+    document.getElementById("boothill").src = "https://s1.zerochan.net/Boothill.600.4168815.jpg"
+    document.getElementById("boothill_txt").innerHTML = " Boothil é um cowboy espacial que está investigando a morte de um de seus companheiros, ele acusa uma protagonista de estar mentindo, em penacony nada é verdade"
+    document.getElementById("jingyuan").src = "https://honkailab.com/wp-content/uploads/2023/03/Before-Dawn_honkailab_genshinlab2.webp"
+    document.getElementById("jingyuan_txt").innerHTML = " Jing Yuan é o líder da frota de naves no luofu, apesar de estar sempre com um rosto tranquilo seu passado guarda muitos traumas e suas ações demonstram sua forte vontade de proteger todos"
+    document.getElementById("jingliu").src = "https://images-ng.pixai.art/images/orig/810f2b76-78f6-41b8-9637-ecf52528f58f"
+    document.getElementById("jingliu_txt").innerHTML =" Jingliu é a mestra morta de Jin Yuan, quando inimigos do passado voltam para assombrar seu aprendiz a mestra morta-viva voltará para ensinar uma última lição"
+    document.getElementById("extra").innerHTML = ""
+}
+
+function build(){
+    document.getElementById("boothill").src = "https://pbs.twimg.com/media/GOxUnroXkAExkWl?format=jpg&name=small"
+    document.getElementById("boothill_txt").innerHTML = " Boothill é um DPS do elemento físicofocado em quebrar a fraqueza dos oponentes"
+    document.getElementById("jingyuan").src = "https://upload-os-bbs.hoyolab.com/upload/2024/03/06/17094339/6022b3b42c09f903e5f5407b315e3b8c_5850152129658975877.jpeg?x-oss-process=image%2Fauto-orient%2C0%2Finterlace%2C1%2Fformat%2Cwebp%2Fquality%2Cq_70"
+    document.getElementById("jingyuan_txt").innerHTML = " Jing Yuan é um DPS do elemento raio focado em carregar uma ataque poderoso que causará muito dano"
+    document.getElementById("jingliu").src = "https://preview.redd.it/en-starrail-1-4-jingliu-in-depth-guide-v0-mjjpph2ipltb1.png?width=640&crop=smart&auto=webp&s=2563e8371a84c87249c112d0e41a55f37308abaf"
+    document.getElementById("jingliu_txt").innerHTML = " Jingliu é uma DPS do elemento gelo focada em causar muito dano a 3 oponentes por vez, porém consurmirá HP dos aliados sempre que atacar"
+    document.getElementById("extra").innerHTML = "Builds por <a id='saindo' href='https://www.hoyolab.com/accountCenter/postList?id=17094339'>https://www.hoyolab.com/accountCenter/postList?id=17094339</a>"
+    document.getElementById("saindo").addEventListener('click', ()=>{
+        alert("você está saindo do site")
+    } )
+}
+
+
+function ults(){
+    document.getElementById("boothill").src = "https://wotpack.ru/wp-content/uploads/2024/04/Buthill-v-Honkai-Star-Rail-gajd-i-bild-800x445.jpg"
+    document.getElementById("boothill_txt").innerHTML = " Boothill dispara contra o inimigo causando muito dano e quebrando sua defesa mesmo que o inimigo não tenha fraqueza contra dano físico"
+    document.getElementById("jingyuan").src = "https://preview.redd.it/could-jing-yuan-with-the-lightning-lord-beat-jingliu-v0-qe18cudcji2c1.jpg?width=1080&crop=smart&auto=webp&s=e7c5acd408fde4ed00d8e1ca5e67727e0f6423a9"
+    document.getElementById("jingyuan_txt").innerHTML = " Jing Yuan invoca o deus dos trovões para causar dano a todos os inimigos, isso tambem acelera seu poderoso golpe"
+    document.getElementById("jingliu").src = "https://cdn.gfinityesports.com/images/ncavvykf/gfinityesports/6d054989868a602e6ffb2a30f23c2fcea71cf49f-1920x1080.jpg?rect=0,36,1920,1008&w=1600&h=840&fit=crop&auto=format"
+    document.getElementById("jingliu_txt").innerHTML = " Jingliu canalisa seu poder interno causando dano a 3 inimigos e ficando mais próxima de seu eu antigo"
+    document.getElementById("extra").innerHTML = ""
+}
+
+
